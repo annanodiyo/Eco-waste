8859bf979137b24f505a4ff1213091def1f79d08
Astra-max <odongo724@gmail.com>
2026-05-28 15:30:00 +0300

    feat: add all required endpoints here

--- server/internal/app/app.go
+++ server/internal/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/annanodiyo/Eco-waste/server/internal/handlers"
+	m "github.com/annanodiyo/Eco-waste/server/internal/middleware"
 )
 
 type App struct {
@@ -19,14 +20,35 @@ func NewApp() *App {
 
 func (a *App) middlewares() {
 	a.router.Use(gin.Logger())
+	m.Cors(a.router)
 }
 
 func (a *App) routes() {
 	v1 := a.router.Group("/api/v1")
+	v1.POST("/products", a.handlers.PostProduct)
+	v1.GET("/products/:id", a.handlers.GetProduct)
+	v1.GET("/product/:id/journey", a.handlers.ProductJourney)
+	v1.GET("products?manufacturer=:wallet", a.handlers.ProdManufWallet)
+
+	v1.POST("/dropoffs/scans", a.handlers.DropoffScan)
+	v1.POST("/dropoffs/manual", a.handlers.Manual)
+	v1.GET("/dropoffs?vendor=:wallet", a.handlers.DropoffLogs)
+	v1.GET("/dropoffs/tokens/calculate", a.handlers.PreviewTokens)
+
+	v1.GET("/consumer/:wallet/balance", a.handlers.WalletBallance)
+	v1.GET("/consumer/:wallet/history", a.handlers.ConsumerHistory)
+
+	v1.POST("/recycler/confirm", a.handlers.ConfirmDelivered)
+	v1.GET("/recycler/pending", a.handlers.PendingWastes)
+
+	v1.GET("/stats", a.handlers.Statistics)
+	v1.GET("/admin/assign/role", a.handlers.Assign)
+
 	auth := v1.Group("/auth")
 	v1.GET("/", a.handlers.RegisterUser)
 	auth.POST("/register", a.handlers.RegisterUser)
 }
+
 
 func (a *App) run() {
 	a.middlewares()
