2e5ab7c09dea6ee4d9f7f43c37b6e424d20df8b7
Astra-max <odongo724@gmail.com>
2026-05-28 15:30:59 +0300

    feat: a middleware that implements browser policy

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -1,2 +1,25 @@
 package middleware
 
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Cors(r *gin.Engine) {
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+		AllowMethods: []string{
+			"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Origin", "Content-Type", "Authorization",
+		},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+}
