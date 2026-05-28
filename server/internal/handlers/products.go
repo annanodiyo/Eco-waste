5650d4e4790b7a8713b50b4bfa2788eaf73c19e0
Astra-max <odongo724@gmail.com>
2026-05-28 15:41:42 +0300

    feat: obtained data from item via qr code

---
+++ server/internal/handlers/products.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) PostProduct(c *gin.Context) {}
+
+func (h *Handler) GetProduct(c *gin.Context) {}
+
+func (h *Handler) ProductJourney(c *gin.Context) {}
+
+func (h *Handler) ProdManufWallet(c *gin.Context) {}
